6d85efd866a889f0e5d9ea2171aa0747eedd5d39
Michael Newman <coloradohokie@gmail.com>
2020-02-03 10:00:05 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#FF0000"
+
+function moveDodgerLeft() {
+    let leftNumber = dodger.style.left.replace("px","")
+    let left = parseInt(leftNumber, 10);
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    let rightNumber = dodger.style.left.replace("px","");
+    let right = parseInt(rightNumber, 10);
+    if (right < 200) {
+        dodger.style.left = `${right + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") { moveDodgerLeft();}
+    if (e.key === "ArrowRight") {moveDodgerRight();}
+});
